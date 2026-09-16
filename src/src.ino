84d1d1e00f8411b4f9a2447527b4016461273b95
Michiel Bruijn <michiel@bruijn.nu>
2026-09-16 17:07:40 +0200

    Bump to v1.36
    Includes the GETJOYCHANNELS serial query (previous commit).

--- src/src.ino
+++ src/src.ino
@@ -35,7 +35,7 @@
  GPIO 0: Onboard BOOT button, repurposed as a "next channel" shortcut
  */
 
-char codeVersion[] = "1.35"; // Software revision.
+char codeVersion[] = "1.36"; // Software revision.
 
 //
 // =======================================================================================================
